853ac5c0106d99689e10958602fb41bc7d4d2503
Mustafa Hassib <mustafahassib68@gmail.com>
2024-04-19 19:32:33 +0200

    finish contact us and packages in homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,34 @@
 const faqs = [
   {
-    question: "1- Lorem ipsum dolor sit amet, Lorem ipsum?",
+    question: '1- Lorem ipsum dolor sit amet, Lorem ipsum?',
     answer:
-      "1- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ",
+      '1- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ',
   },
   {
-    question: "2- Lorem ipsum dolor sit amet, Lorem ipsum?",
+    question: '2- Lorem ipsum dolor sit amet, Lorem ipsum?',
     answer:
-      "2- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ",
+      '2- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ',
   },
   {
-    question: "3- Lorem ipsum dolor sit amet, Lorem ipsum?",
+    question: '3- Lorem ipsum dolor sit amet, Lorem ipsum?',
     answer:
-      "3- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ",
+      '3- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ',
   },
   {
-    question: "4- Lorem ipsum dolor sit amet, Lorem ipsum?",
+    question: '4- Lorem ipsum dolor sit amet, Lorem ipsum?',
     answer:
-      "4- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ",
+      '4- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ',
   },
   {
-    question: "5- Lorem ipsum dolor sit amet, Lorem ipsum?",
+    question: '5- Lorem ipsum dolor sit amet, Lorem ipsum?',
     answer:
-      "5- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ",
+      '5- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid.Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur ',
   },
 ];
 
 const isMobile = window.innerWidth < 576;
-const tabs = document.querySelector(".faq-container .tabs");
-const answersBody = document.querySelector(".faq-container .body");
+const tabs = document.querySelector('.faq-container .tabs');
+const answersBody = document.querySelector('.faq-container .body');
 faqs.forEach((faq, index) => {
   tabs.innerHTML += `
     <li class="tab"> 
@@ -55,39 +55,39 @@ faqs.forEach((faq, index) => {
   }
 });
 
-const answers = document.querySelectorAll(".faq-container .content");
-const tabButton = document.querySelectorAll(".faq-container .tabs .tab");
-tabButton[0].classList.add("active");
-answers.length !== 0 && answers[0].classList.add("active-tab");
+const answers = document.querySelectorAll('.faq-container .content');
+const tabButton = document.querySelectorAll('.faq-container .tabs .tab');
+tabButton[0].classList.add('active');
+answers.length !== 0 && answers[0].classList.add('active-tab');
 tabButton.forEach((button, index) => {
-  button.addEventListener("click", () => toggleTab(button, index + 1));
+  button.addEventListener('click', () => toggleTab(button, index + 1));
 });
 
 function toggleTab(selectedTabId, tabIndex) {
   tabButton.forEach((button) => {
-    button.classList.remove("active");
+    button.classList.remove('active');
   });
   const selectedTab = selectedTabId;
-  selectedTab.classList.add("active");
+  selectedTab.classList.add('active');
 
   answers.forEach((answer) => {
-    answer.classList.remove("active-tab");
+    answer.classList.remove('active-tab');
   });
 
   const activeTab = document.querySelector(`#tab${tabIndex}`);
-  activeTab.classList.add("active-tab");
+  activeTab.classList.add('active-tab');
 }
 
-const navbarToggler = document.querySelector("#navbar-toggle");
-const navbarList = document.querySelector(".navbar");
+const navbarToggler = document.querySelector('#navbar-toggle');
+const navbarList = document.querySelector('.navbar');
 const body = document.body;
-navbarToggler.addEventListener("click", () => {
-  if (navbarList.classList.contains("active-nav")) {
-    navbarList.classList.remove("active-nav");
-    body.style.overflow = "auto";
+navbarToggler.addEventListener('click', () => {
+  if (navbarList.classList.contains('active-nav')) {
+    navbarList.classList.remove('active-nav');
+    body.style.overflow = 'auto';
   } else {
-    navbarList.classList.add("active-nav");
-    body.style.overflow = "hidden";
+    navbarList.classList.add('active-nav');
+    body.style.overflow = 'hidden';
   }
 });
 
@@ -101,10 +101,33 @@ gsap.registerPlugin(ScrollTrigger);
 
 const lenis = new Lenis();
 
-lenis.on("scroll", ScrollTrigger.update);
+lenis.on('scroll', ScrollTrigger.update);
 
 gsap.ticker.add((time) => {
   lenis.raf(time * 1000);
 });
 
 gsap.ticker.lagSmoothing(0);
+
+function toggleContent(clickedButton) {
+  clickedButton.classList.toggle('rotate');
+
+  const allButtons = document.querySelectorAll('.toggle-section');
+  const allContents = document.querySelectorAll('.whats-included__body');
+  console.log(allButtons);
+  allContents.forEach((content) => {
+    if (clickedButton.dataset.target === `#${content.id}`) {
+      content.style.display =
+        content.style.display === 'block' ? 'none' : 'block';
+    } else {
+      content.style.display = 'none';
+    }
+  });
+}
+
+// Add click event listeners to all toggle buttons
+document.querySelectorAll('.toggle-collapse').forEach((button) => {
+  button.addEventListener('click', function () {
+    toggleContent(this);
+  });
+});
--- assets/js/sliders-configs.js
+++ assets/js/sliders-configs.js
@@ -1,5 +1,5 @@
 const options = {
-  type: "loop",
+  type: 'loop',
   arrows: false,
   pagination: false,
   perPage: 8,
@@ -13,53 +13,24 @@ const options = {
     speed: 1,
   },
 };
-document.addEventListener("DOMContentLoaded", function () {
-  const splide1 = new Splide(".brands-slides__top__slider", options);
-  const splide2 = new Splide(".brands-slides__bottom__slider", {
+document.addEventListener('DOMContentLoaded', function () {
+  const splide1 = new Splide('.brands-slides__top__slider', options);
+  const splide2 = new Splide('.brands-slides__bottom__slider', {
     ...options,
     autoScroll: { speed: -1 },
   });
-  const splide3 = new Splide(".latest-projects", {
+  const splide3 = new Splide('.latest-projects', {
     ...options,
     perPage: 4.5,
-    gap: "7rem",
+    gap: '7rem',
     snap: true,
     interval: 1000,
     autoScroll: {},
-    lazyLoad: "sequential",
+    lazyLoad: 'sequential',
     breakpoints: {
       640: { perPage: 2.5 },
       1200: { perPage: 3 },
       1600: { perPage: 3.5 },
-    },
-  });
-
-  const splide4 = new Splide(".testimonials_slider", {
-    ...options,
-    perPage: 4,
-    interval: 3000,
-    autoplay: true,
-    autoScroll: null,
-    focus: "center",
-    breakpoints: {
-      640: {
-        perPage: 1,
-        gap: "1rem",
-      },
-    },
-  });
-  const splide5 = new Splide(".packeges-services", {
-    ...options,
-    perPage: 3,
-    interval: 3000,
-    autoplay: true,
-    autoScroll: null,
-    focus: "center",
-    breakpoints: {
-      640: {
-        perPage: 1,
-        gap: "1rem",
-      },
     },
   });
 
@@ -68,6 +39,4 @@ document.addEventListener("DOMContentLoaded", function () {
   for (var i = 0; i < elms.length; i++) {
     elms[i].mount(window.splide.Extensions);
   }
-  splide4.mount();
-  splide5.mount();
 });
